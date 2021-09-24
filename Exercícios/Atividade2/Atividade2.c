d1fa00627fe489128a491fa1cf9066ce356bb801
faffonso <francisco.affonso02@gmail.com>
2021-09-24 15:35:26 -0300

    Adicionando insertion completo + inicio merge

--- Exercícios/Atividade2/Atividade2.c
+++ Exercícios/Atividade2/Atividade2.c
@@ -16,20 +16,67 @@ void printMatrix (int n, int m[], int max, int matrix[n][max]){
 }
 
 void insertionSort (int vector[], int n) {
-    int countChanges = 0, 
-        countComparison = 0, 
-        left, j;
-    for (int i=0; i<n; i++) {
+    int countChanges = 0,
+    countComparison = 0,
+    left, j;
+    for (int i=1; i<n; i++) {
         j = i;
         left = i - 1;
-        while (j > 0 && vector[j] < vector[left]) {
+        while (left >= 0 && vector[j] < vector[left]) {
             swap (&vector[left], &vector[j]);
             j--;
             left = j - 1;
             countChanges++;
+            if (left >= 0)
+                countComparison++;
         }
+        countComparison++;
+        countChanges += 2;
     }
     printf ("I %i %i %i\n", n, countChanges, countComparison);
+}
+
+void merge (int vector[], int begin, int mid, int end) {
+    int aux1 = begin;
+    int aux2 = mid + 1;
+    int vectorAux[end - begin + 1];
+    int i = 0;
+    while (aux1 <= mid && aux2 <= end) {
+        if (vector[aux1] <= vector[aux2]) {
+            vectorAux[i] = vector[aux1];
+            aux1++;
+            i++;
+        } else {
+            vectorAux[i] = vector[aux2];
+            aux2++;
+            i++;
+        }
+    }
+
+    while (aux1 <= mid) {
+        vectorAux[i] = vector[aux1];
+        i++;
+        aux1++;
+    }
+    
+    while (aux2 <= end) {
+        vectorAux[i] = vector[aux2];
+        i++;
+        aux2++;
+    }
+
+    for (int i=begin, j=0; i<=end; i++, j++) {
+        vector[i] = vectorAux[j];
+    }
+}
+
+void mergeSort (int vector[], int begin, int end) {
+    if (begin >= end)
+        return;
+    int mid = (begin + end) / 2;
+    mergeSort (vector, begin, mid);
+    mergeSort (vector, mid + 1, end);
+    merge (vector, begin, mid, end);
 }
 
 int main (int argc, char *argv[]) {
@@ -40,7 +87,7 @@ int main (int argc, char *argv[]) {
     for (int i=0; i<numVector; i++) {
         scanf("%i", &tamVector[i]);
         if (i==0 || tamVector[i] > max)
-            max = tamVector[i];   
+            max = tamVector[i];
     }
 
     int matrix[numVector][max];
@@ -49,11 +96,11 @@ int main (int argc, char *argv[]) {
             scanf("%i", &matrix[i][j]);
         }
     }
-    
+
     printMatrix(numVector, tamVector, max, matrix);
     for (int i=0; i<numVector; i++) {
-        insertionSort(matrix[i], tamVector[i]);
+        //insertionSort(matrix[i], tamVector[i]);
+        mergeSort(matrix[i], 0, tamVector[i] - 1);
     }
     printMatrix(numVector, tamVector, max, matrix);
-
 }
